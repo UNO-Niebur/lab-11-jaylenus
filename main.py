3e2ac7d0bfd92f59b01ab3c0abae2fc08ae0b051
jaylenus <jaylenatsou@icloud.com>
2026-04-15 20:38:57 +0000

    Complete Lab 11

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # StopLightSim.py
-# Name:
-# Date:
-# Assignment:
+# Name: Jaylen Atsou
+# Date: April 15, 2026
+# Assignment: Lab 11
 
 import simpy
 
@@ -28,12 +28,16 @@ def stopLight(env):
 
 def car(env, car_id):
     """Simulates a car arriving and waiting for the light."""
+    global greenLight
     
     print("Car", car_id, "arrived at", env.now)
 
-    # TODO: Make the car wait while the light is red
-    # Hint: use a loop and env.timeout(1)
+    # Make the car wait while the light is red
+    while not greenLight:
+        print("Car", car_id, "waiting at", env.now)
+        yield env.timeout(1)
 
+    yield env.timeout(1)
     print("Car", car_id, "departed at", env.now)
 
 
@@ -46,7 +50,8 @@ def carArrival(env):
         car_id += 1
         print("Creating Car", car_id)
 
-        # TODO: Start a new car process
+        # Start a new car process
+        env.process(car(env, car_id))
 
         yield env.timeout(5)
 
@@ -57,7 +62,8 @@ def main():
     # Start processes
     env.process(stopLight(env))
     
-    # TODO: Start the carArrival process
+    # Start the carArrival process
+    env.process(carArrival(env))
 
     # Run simulation
     env.run(until=100)
